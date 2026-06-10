fd9dab01223197851053a748648e629c4884b85c
elnetkrakow <elnetkrakow@gmail.com>
2026-06-10 20:00:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 const SUPABASE_URL = "https://ebguhxeywwsmqbvnfhnp.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_JHiOY_XRueQ6R1ApozzfEA_ujc6ymvy";
-const APP_VERSION = "2026.06.10-27";
+const APP_VERSION = "2026.06.10-28-STABILNA-EDYCJA";
 
 let accessToken = localStorage.getItem("elnet_token") || null;
 let zalogowanyUser = null;
